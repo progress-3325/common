0353ecae5d8b53ca25e2099dfa87b1dfeda4582b
progress-3325 <progress.3325ml@gmail.com>
2024-12-28 00:04:44 +0200

    put every header into one library

---
+++ src/CCommon.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// common game engine library
+
+// for use by common apps
+
+#include "Common/application.h"
+// ---------------Entry Point------------------
+#include "Common/entry_point.h"
+// --------------------------------------------
